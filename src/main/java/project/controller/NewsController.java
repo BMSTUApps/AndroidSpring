9d9a8a1a810292c5d6881a37332c1d19d0990400
smet_k <ski97@mail.ru>
2019-05-14 02:19:20 +0300

    insert news

--- src/main/java/project/controller/NewsController.java
+++ src/main/java/project/controller/NewsController.java
@@ -2,12 +2,10 @@ package project.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import project.dao.NewsDAO;
 import project.model.DAOResponse;
+import project.model.News;
 
 @ResponseBody
 @RestController
@@ -25,5 +23,15 @@ public class NewsController {
             return ResponseEntity.status(HttpStatus.OK).body(daoResponse.body);
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Some error happened while getting news");
+    }
+
+    @RequestMapping(path = "/news", method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity<?> insertNews(@RequestBody News body) {
+        DAOResponse daoResponse = newsDAO.insertNews(body);
+        if (daoResponse.status == HttpStatus.OK) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(daoResponse.body);
+        }
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Unsuccess stair link inset");
     }
 }
--- src/main/java/project/dao/NewsDAO.java
+++ src/main/java/project/dao/NewsDAO.java
@@ -2,10 +2,13 @@ package project.dao;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Service;
 import project.model.DAOResponse;
+import project.model.MapStairsLink;
 import project.model.News;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 
 @Service
@@ -27,6 +30,29 @@ public class NewsDAO {
             result.body = null;
             result.status = HttpStatus.NOT_FOUND;
         }
+        return result;
+    }
+
+    public DAOResponse insertNews(News news) {
+        DAOResponse<News> result = new DAOResponse<>();
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+        try {
+            template.update(connection -> {
+                PreparedStatement statement = connection.prepareStatement("INSERT INTO news (title, payload) VALUES(?, ?);",
+                        PreparedStatement.RETURN_GENERATED_KEYS);
+                statement.setString(1, news.getTitle());
+                statement.setString(2, news.getPayload());
+
+                return statement;
+            }, keyHolder);
+            result.body = news;
+            result.status = HttpStatus.OK;
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.body = null;
+            result.status = HttpStatus.CONFLICT;
+        }
+
         return result;
     }
 }
